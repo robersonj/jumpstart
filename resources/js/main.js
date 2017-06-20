75e1629e5bb6c9bdce4779dd0a7d0e48dd3a437b
Jessica Roberson <Jessica Roberson>
2017-06-19 22:37:38 -0400

    Added other jumbotrons.

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    slidesToScroll: 1,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
